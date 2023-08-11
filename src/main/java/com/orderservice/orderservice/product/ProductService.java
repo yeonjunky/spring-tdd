8730ddcb1e6d5f106e80c3cad1f1de753ef7e875
yeonjunky <yeonjunky@gmail.com>
2023-08-11 20:06:19 +0900

    Api로 전환하고 테스트

--- src/main/java/com/orderservice/orderservice/product/ProductService.java
+++ src/main/java/com/orderservice/orderservice/product/ProductService.java
@@ -1,9 +1,16 @@
 package com.orderservice.orderservice.product;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 public class ProductService {
     private final ProductPort productPort;
 
@@ -11,10 +18,13 @@ public class ProductService {
         this.productPort = productport;
     }
 
-    public void addProduct(final AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
         this.productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
---
+++ src/test/java/com/orderservice/orderservice/product/ProductApiTest.java
@@ -0,0 +1,45 @@
+package com.orderservice.orderservice.product;
+
+import com.orderservice.orderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+
+class ProductApiTest extends ApiTest {
+    
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void registerProduct() {
+        final AddProductRequest request = 상품등록요청_생성();
+
+        final ExtractableResponse<Response> response = 상품등록요청(request);
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+    }
+
+    private static AddProductRequest 상품등록요청_생성() {
+        final String name = "productname";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
+        return request;
+    }
+}
