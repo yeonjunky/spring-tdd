591f03377626945d66947163f8ed131543592fe9
yeonjunky <yeonjunky@gmail.com>
2023-08-12 13:18:44 +0900

    엔티티로 만들고 쓸모없는 메서드 삭제

--- src/main/java/com/orderservice/orderservice/product/Product.java
+++ src/main/java/com/orderservice/orderservice/product/Product.java
@@ -1,14 +1,25 @@
 package com.orderservice.orderservice.product;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Product {
-    private final String name;
-    private final int price;
-    private final DiscountPolicy discountPolicy;
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
 
-    Product(final String name, final int price, final DiscountPolicy discountPolicy) {
+    private String name;
+    private int price;
+    private DiscountPolicy discountPolicy;
+
+    public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
         Assert.hasText(name, "상품명은 필수입니다.");
         Assert.isTrue(price > 0, "상품 가격은 0보다 커야합니다.");
         Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
@@ -16,13 +27,5 @@ class Product {
         this.price = price;
         this.discountPolicy = discountPolicy;
 
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
