4c7af65f27f92b3fbb10846b9b1c1ba6522858fe
yeonjunky <yeonjunky@gmail.com>
2023-08-12 13:20:13 +0900

    change to interface and extend JpaRepository

--- src/main/java/com/orderservice/orderservice/product/ProductRepository.java
+++ src/main/java/com/orderservice/orderservice/product/ProductRepository.java
@@ -3,17 +3,10 @@ package com.orderservice.orderservice.product;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public class ProductRepository {
-    private Map<Long, Product> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(final Product product) {
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
-    
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
