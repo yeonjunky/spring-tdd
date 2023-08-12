358f132ac9093597987942682ea5b4318da5e930
yeonjunky <yeonjunky@gmail.com>
2023-08-12 13:19:39 +0900

    remove unused import and add transactional annotation

--- src/main/java/com/orderservice/orderservice/product/ProductService.java
+++ src/main/java/com/orderservice/orderservice/product/ProductService.java
@@ -2,7 +2,6 @@ package com.orderservice.orderservice.product;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +18,7 @@ public class ProductService {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
