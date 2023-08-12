ad3ae8b3b98bea30ccb074e2360c1791d79aa28b
yeonjunky <yeonjunky@gmail.com>
2023-08-12 13:50:28 +0900

    상품 조회 기능 추가

--- src/main/java/com/orderservice/orderservice/product/ProductPort.java
+++ src/main/java/com/orderservice/orderservice/product/ProductPort.java
@@ -2,4 +2,6 @@ package com.orderservice.orderservice.product;
 
 public interface ProductPort {
     void save(final Product product);
+
+    Product getProduct(Long productId);
 }
---
+++ src/test/java/com/orderservice/orderservice/product/ProductSteps.java
@@ -0,0 +1,26 @@
+package com.orderservice.orderservice.product;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+public class ProductSteps {
+    public static AddProductRequest 상품등록요청_생성() {
+        final String name = "productname";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
+        return request;
+    }
+
+    public static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+    }
+}
