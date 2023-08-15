bbf71c72cd62685919e5bdea2994c319f5bad2e8
yeonjunky <yeonjunky@gmail.com>
2023-08-15 14:43:25 +0900

    스프링 부트 테스트로 수정

--- src/test/java/com/orderservice/orderservice/product/ProductServiceTest.java
+++ src/test/java/com/orderservice/orderservice/product/ProductServiceTest.java
@@ -6,34 +6,32 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class ProductServiceTest {
 
+    @Autowired
     private ProductPort productPort;
+    @Autowired
     private ProductService productService;
-
-    @BeforeEach
-    void setUp() {
-        productPort = Mockito.mock(ProductPort.class);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     void 상품수정() {
-
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품수정", 2000, DiscountPolicy.NONE);
-        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         productService.updateProduct(productId, request);
 
-        Assertions.assertThat(product.getName()).isEqualTo("상품수정");
-        Assertions.assertThat(product.getPrice()).isEqualTo(2000);
+        final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        GetProductResponse productResponse = response.getBody();
+        Assertions.assertThat(productResponse.name()).isEqualTo("상품수정");
+        Assertions.assertThat(productResponse.price()).isEqualTo(2000);
 
     }
 
