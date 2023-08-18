0cc9e54539331b6a7483b0a4a4757ecf2b30c93c
yeonjunky <yeonjunky@gmail.com>
2023-08-18 19:39:37 +0900

    JPA 적용 후 테스트

--- src/main/java/com/orderservice/orderservice/order/OrderRepository.java
+++ src/main/java/com/orderservice/orderservice/order/OrderRepository.java
@@ -1,17 +1,9 @@
 package com.orderservice.orderservice.order;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Repository
-public class OrderRepository {
-    private Map<Long, Order> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(Order order) {
-        order.assignId(++sequence);
-        persistence.put(order.getId(), order);
-    }
+interface OrderRepository extends JpaRepository<Order, Long> {
 }
